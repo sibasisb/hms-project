cfdc04c80fa8a66848ed53d27db9c66bc31f4499
HellBringer419 <hell_bringer419@outlook.com>
2021-02-20 16:34:51 +0530

    added route /appointment to app.js

--- frontend/hospital-management-frontend/src/App.js
+++ frontend/hospital-management-frontend/src/App.js
@@ -1,37 +1,38 @@
-import React, { createContext, useContext, useEffect, useReducer } from 'react'
-import {Switch,BrowserRouter,Route, useHistory} from 'react-router-dom'
-import './App.css';
-import MenuComponent from './Components/MenuComponent';
-import userReducer, { initialState } from './Reducers/userReducer';
+import React, { createContext, useContext, useEffect, useReducer } from "react";
+import { Switch, BrowserRouter, Route, useHistory } from "react-router-dom";
+import "./App.css";
+import Appointment from "./Components/Appointment";
+import MenuComponent from "./Components/MenuComponent";
+import userReducer, { initialState } from "./Reducers/userReducer";
 
-export const UserContext=createContext()
+export const UserContext = createContext();
 
-const Routing=()=>{
-  const {state,dispatch}=useContext(UserContext)
-  const history=useHistory()
-  useEffect(()=>{
-    
-  },[])
-  
-  return (
-    <Switch>
-      
-    </Switch>
-  )
-}
+const Routing = () => {
+	const { state, dispatch } = useContext(UserContext);
+	const history = useHistory();
+	useEffect(() => {}, []);
+
+	return (
+		<Switch>
+			<Route path="/appointment">
+				<Appointment />
+			</Route>
+		</Switch>
+	);
+};
 
 function App() {
-  const [state,dispatch]=useReducer(userReducer,initialState)
-  return (
-    <div>
-      <UserContext.Provider value={{state,dispatch}}>
-        <BrowserRouter>
-          <MenuComponent/>
-          <Routing/>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </div>
-  );
+	const [state, dispatch] = useReducer(userReducer, initialState);
+	return (
+		<div>
+			<UserContext.Provider value={{ state, dispatch }}>
+				<BrowserRouter>
+					<MenuComponent />
+					<Routing />
+				</BrowserRouter>
+			</UserContext.Provider>
+		</div>
+	);
 }
 
 export default App;
