aa5d63a5d871525a9b3a13d0838fba3a3b958497
HellBringer419 <hell_bringer419@outlook.com>
2021-02-20 16:30:28 +0530

    added /appointment route to MenuComponent

--- frontend/hospital-management-frontend/src/Components/MenuComponent.js
+++ frontend/hospital-management-frontend/src/Components/MenuComponent.js
@@ -15,7 +15,9 @@ const MenuComponent=()=>{
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">  
                 <ul className="navbar-nav mr-auto">  
-                
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/appointment"> Book Appointment </Link>
+                    </li>
                 </ul>
                 </div>
             </nav>
