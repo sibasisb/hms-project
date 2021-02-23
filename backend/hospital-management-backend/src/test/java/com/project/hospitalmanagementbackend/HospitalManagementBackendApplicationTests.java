bc3f918703454775d9565ac682fd478c83b345e8
HellBringer419 <hell_bringer419@outlook.com>
2021-02-23 18:28:22 +0530

    Added Spring

---
+++ backend/hospital-management-backend/src/test/java/com/project/hospitalmanagementbackend/HospitalManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hospitalmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
