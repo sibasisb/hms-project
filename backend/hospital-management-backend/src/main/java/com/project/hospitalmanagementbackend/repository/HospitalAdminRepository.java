995bf24b65cc5d3cad41c24f3fec79456644fae7
Dipan <chakrabortydipan001@gmail.com>
2021-02-24 10:19:28 +0530

    changed package name

--- backend/hospital-management-backend/src/main/java/com/project/hospitalmanagementbackend/repository/HospitalAdminRepository.java
+++ backend/hospital-management-backend/src/main/java/com/project/hospitalmanagementbackend/repository/HospitalAdminRepository.java
@@ -1,4 +1,4 @@
-package com.project.hospitalmanagementbackend.repositories;
+package com.project.hospitalmanagementbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
