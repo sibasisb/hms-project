ab64a645532c836158ad0bf63c2681588db59ca9
sibasisb <sibasisbhattacharjee@gmail.com>
2021-02-24 16:49:03 +0530

    Adding result infos

--- backend/hospital-management-backend/src/main/java/com/project/hospitalmanagementbackend/model/TestResultsInformation.java
+++ backend/hospital-management-backend/src/main/java/com/project/hospitalmanagementbackend/model/TestResultsInformation.java
@@ -1,7 +1,5 @@
 package com.project.hospitalmanagementbackend.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,27 +19,23 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="test_result")
-public class TestResult {
+@Table(name="result_infos")
+public class TestResultsInformation {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="result_id")
-	private long resultId;
+	@Column(name="result_info_id")
+	private Long resultInfoId;
 	
-	@Column(name="test_name")
-	private String testName;
+	@Column(name="result_info_name")
+	private String resultInfoName;
+	
+	@Column(name="result_info_value")
+	private String resultInfoValue;
 	
 	@ManyToOne
-	@JoinColumn(name="result_patient_id")
-	private Patient patient;
+	@JoinColumn(name="test_result_id")
+	private TestResult testResult;
 	
-	@OneToOne 
-	@JoinColumn(name="result_appointment_id") 
-	private Appointment appointment;
-	
-	@Column(name = "results")
-	@OneToMany(mappedBy="testResult")
-	private List<TestResultsInformation> infos;;
 	
 }
