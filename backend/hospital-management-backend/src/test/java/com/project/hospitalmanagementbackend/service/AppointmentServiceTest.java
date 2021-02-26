3a63048d3e22b139fc30875983b36b4533df8fdc
sibasisb <sibasisbhattacharjee@gmail.com>
2021-02-26 19:33:08 +0530

    Added test for us_13

--- backend/hospital-management-backend/src/test/java/com/project/hospitalmanagementbackend/service/AppointmentServiceTest.java
+++ backend/hospital-management-backend/src/test/java/com/project/hospitalmanagementbackend/service/AppointmentServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.project.hospitalmanagementbackend.dto.AppointmentInfo;
 import com.project.hospitalmanagementbackend.model.Appointment;
 import com.project.hospitalmanagementbackend.model.Doctor;
 import com.project.hospitalmanagementbackend.model.Facility;
@@ -63,13 +64,25 @@ public class AppointmentServiceTest {
 		Doctor doctor = new Doctor("DOC001", "M. B. B. S.", "cardio", 5, "Monday", "5:00", new BigDecimal(250.00), null , new User(2l, "Munna", "Bhai", LocalDate.of(1968, 8, 4), "male", "8459872650", "munna@bhai.mbbs", "circiut", "Doctor"));
 		Facility facility = new Facility(4l, "this", null);
 		HospitalFacility hospitalFacility = new HospitalFacility(3l, hospital, facility , "desc", "rem", new BigDecimal(54.00));
-		Appointment appointment = new Appointment(121l, patient, doctor , hospital, hospitalFacility , LocalDate.of(2021, 02, 14), LocalTime.of(20, 04), "hem", null, true, false);
-		List<Appointment> appointmentList=new ArrayList<Appointment>();
-		appointmentList.add(appointment);
+		Appointment a = new Appointment(121l, patient, doctor , hospital, hospitalFacility , LocalDate.of(2021, 02, 14), LocalTime.of(20, 04), "hem", null, true, false);
+		List<AppointmentInfo> appointmentInfoList = new ArrayList<AppointmentInfo>();
+		List<Appointment> appointments=appointmentRepository.findPatientsWithFacilityRequests(hospital.getHospitalId());
+		appointments.forEach((appointment->{
+			AppointmentInfo appointmentInfo = new AppointmentInfo();
+			appointmentInfo.setAppointmentDate(appointment.getAppointmentDate());
+			appointmentInfo.setAppointmentTime(appointment.getAppointmentTime());
+			if(appointment.getHospitalFacility()==null)
+			appointmentInfo.setDoctorName(appointment.getDoctor().getUser().getFirstName()+" "+appointment.getDoctor().getUser().getLastName());
+			else
+			appointmentInfo.setFacilityName(appointment.getHospitalFacility().getFacility().getName());
+			
+			appointmentInfo.setHospitalName(appointment.getHospital().getName());
+			appointmentInfoList.add(appointmentInfo);
+		}));
 		String hospitalAdminId="HAD00001";
-    	when(appointmentRepository.findPatientsWithFacilityRequests(hospital.getHospitalId())).thenReturn(appointmentList);
+    	when(appointmentRepository.findPatientsWithFacilityRequests(hospital.getHospitalId())).thenReturn(appointments);
     	when(hospitalAdminRepository.getHospitalIdByAdminId(hospitalAdminId)).thenReturn(hospital.getHospitalId());
-    	assertEquals(appointmentService.getPendingAppointents(hospitalAdminId),appointmentList);
+    	assertEquals(appointmentService.getPendingAppointents(hospitalAdminId),appointmentInfoList);
     }
 	
 	@Test
