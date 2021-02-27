fc216bab23d3fb49fb55c49b1f945692320af951
HellBringer419 <hell_bringer419@outlook.com>
2021-02-27 11:51:18 +0530

    Add View appointmnet by Hospital Admin

---
+++ backend/hospital-management-backend/src/main/java/com/project/hospitalmanagementbackend/exception/HospitalAdminNotFoundException.java
@@ -0,0 +1,14 @@
+package com.project.hospitalmanagementbackend.exception;
+
+public class HospitalAdminNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4193477273307379724L;
+
+	public HospitalAdminNotFoundException(String message) {
+		super(message);
+	}
+
+}
