fe8e44a23b9ad41100c7a51ea25e31b235dbfdeb
sibasisb <sibasisbhattacharjee@gmail.com>
2021-03-02 13:03:10 +0530

    adding a set

--- frontend/hospital-management-frontend/src/helpers/Unauthorized.js
+++ frontend/hospital-management-frontend/src/helpers/Unauthorized.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Unauthorized() {
     return (
         <div  className="text-center">
-            <h1>403 unauthorized</h1>
+            <h1>401 unauthorized</h1>
         </div>
     )
 }
